020292eadf3555eb1d7e930da34cb5a1195e6b59
salpalvv <gth701m@gmail.com>
2016-12-18 23:28:24 -0500

    finished day 3 part 1

--- src/lib/day3.rs
+++ src/lib/day3.rs
@@ -1,9 +1,48 @@
 pub fn solve(input: &str) -> i32 {
-    1
+    let mut possible = 0;
+
+    for line in input.lines() {
+        let parsed_line = parse_line(line);
+        let parsed_lengths: Result<Vec<i32>, _> = parsed_line.iter().map(|&x| x.parse::<i32>()).collect();
+        let parsed_lengths = parsed_lengths.expect("couldn't parse lines");
+        if is_possible(&parsed_lengths) {
+            possible += 1;
+        }
+    }
+    possible
+}
+
+fn is_possible(input: &[i32]) -> bool {
+    if input[0] < input[1] + input[2] {
+        if input[1] < input[0] + input[2] {
+            if input[2] < input[1] + input[0] {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn parse_line(line: &str) -> Vec<&str> {
+    let parsed = line.split_whitespace().collect();
+    parsed
 }
 
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_parsed_line(){
+        let input = " 1 2 3 ";
+        assert_eq!(parse_line(&input), ["1","2","3"])
+
+    }
+
+    #[test]
+    fn test_is_possible(){
+        let input = [4,5,6];
+        assert_eq!(is_possible(&input),true);
+    }
 
     #[test]
     fn sample() {
